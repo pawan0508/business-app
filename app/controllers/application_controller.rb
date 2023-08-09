b5acb247ad3cc8b768ad7346a347b44d2a0c584d
Pawan Shah <pawan@techwinners.in>
2023-08-09 17:29:45 +0530

    Implemented authentication and swagger documentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,31 @@ class ApplicationController < ActionController::API
     current_api_v1_user
   end
 
+  def authenticate_user!
+    authenticate_api_v1_user!
+  end
+  
+  def authenticate_api_v1_user!
+    token_payload = decoded_token
+    if token_payload.nil? || token_payload['exp'].to_i < Time.now.to_i
+      render json: { error: 'Token expired or invalid. Please sign in again.' }, status: :unauthorized
+    else
+      @current_user = User.find(token_payload['user_id'])
+    end
+  end
+
+  private
+
+  def decoded_token
+    token = request.headers['Authorization']&.split&.last
+    return nil unless token
+
+    begin
+      decoded_payload, _ = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+      decoded_payload
+    rescue JWT::DecodeError, JWT::ExpiredSignature
+      nil
+    end
+  end
+
 end
